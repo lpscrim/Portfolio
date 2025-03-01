e672b7238397bdd5b07015823cfe3d690be4f89a
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-03-01 18:15:33 +0000

    changed layout and resolved tailwind config

--- portfolio/src/hero.ts
+++ portfolio/src/hero.ts
@@ -1,9 +1,10 @@
 
 document.querySelector<HTMLDivElement>('#hero')!.innerHTML = `
   <div>
-    <p>
+    <p class="text-2xl font-bold p-50 w-full text-center">
       Hello, I'm <strong>John Doe</strong>, a software engineer based in New York City.
     </p>
+
   </div>
 `
 
--- portfolio/src/theme.ts
+++ portfolio/src/theme.ts
@@ -1,6 +1,5 @@
-export function theme(element: HTMLButtonElement) {
   const setTheme = (className) => {
   var root = document.getElementsByTagName('html')[0];
   root.className = className;
 }
-}
+
