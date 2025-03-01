5a390043a1ac6fc017176685773705df402b5cb6
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-03-01 17:56:47 +0000

    Initial commit of portfolio proj with basic outline with VanillaJS and tailwindCSS

---
+++ portfolio/src/hero.ts
@@ -0,0 +1,9 @@
+
+document.querySelector<HTMLDivElement>('#hero')!.innerHTML = `
+  <div>
+    <p>
+      Hello, I'm <strong>John Doe</strong>, a software engineer based in New York City.
+    </p>
+  </div>
+`
+
---
+++ portfolio/src/theme.ts
@@ -0,0 +1,6 @@
+export function theme(element: HTMLButtonElement) {
+  const setTheme = (className) => {
+  var root = document.getElementsByTagName('html')[0];
+  root.className = className;
+}
+}
