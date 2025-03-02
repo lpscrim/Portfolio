071e8d9dd561953346b7dd68ff8e4d3dfaba686e
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-03-02 21:39:17 +0000

    Added animation

--- portfolio/src/hero.ts
+++ portfolio/src/hero.ts
@@ -8,3 +8,73 @@ document.querySelector<HTMLDivElement>('#hero')!.innerHTML = `
   </div>
 `
 
+//replace text here with a function that takes the innerHTML from the main doc and animates it plus additional text from here//
+//Lewis Scrimgeour//
+                    //School taught Architectural Engineer//
+                    //Self taught English Teacher//
+                    //Self Taught Chef//
+                    //Self taught Photographer//
+                    //self taught Music producer//
+                    //Self taught Artist//                                   
+// Self taught web developer//
+
+
+
+  const typeElement = document.querySelector<HTMLDivElement>('#typeTest')!;
+  const typeOriginal = typeElement.innerHTML;
+  const texts = [
+    'School taught Architectural Engineer',
+    'Self taught English Teacher',
+    'Self Taught Chef',
+    'Self taught Photographer',
+    'Self taught Music producer',
+    'Self taught Artist',
+    typeOriginal
+  ];
+
+  function typewriter(element: HTMLDivElement, text: string, callback?: () => void): void {
+    const textArray = text.split('');
+    element.innerHTML = '';
+    textArray.forEach((letter: string, i) => {
+      setTimeout(() => {
+        element.innerHTML += letter;
+        if (i === textArray.length - 1 && callback) {
+          setTimeout(callback, 1000); 
+        }
+      }, 50 * i);
+    });
+  }
+  
+  function deleteText(element: HTMLDivElement, callback?: () => void): void {
+    const textArray = element.innerHTML.split('');
+    textArray.forEach((_, i) => {
+      setTimeout(() => {
+        textArray.pop();
+        element.innerHTML = textArray.join('');
+        if (textArray.length === 0 && callback) {
+          setTimeout(callback, 500); 
+        }
+      }, 20 * i);
+    });
+  }
+  
+  function typeSequence(element: HTMLDivElement, texts: string[]) {
+    let index = 0;
+  
+    function next() {
+      if (index < texts.length) {
+        if (index === texts.length - 1) {
+          typewriter(element, texts[index]);
+        } else {
+          typewriter(element, texts[index], () => {
+            deleteText(element, next);
+          });
+        }
+        index++;
+      }
+    }
+  
+    next();
+  }
+  
+  typeSequence(typeElement, texts);
