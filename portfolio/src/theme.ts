1899267849e8e586f20278e793be422fefef931f
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-03-04 23:48:47 +0000

    Added two themes- Black/White

--- portfolio/src/theme.ts
+++ portfolio/src/theme.ts
@@ -1,12 +1,15 @@
   export const  setTheme = () => {
   var root = document.getElementsByTagName('html')[0];
   if (root.className == null) {
+    root.className = 'light'
+  } else if (root.className === 'light') {
     root.className = 'dark'
   } else if (root.className === 'dark') {
+    root.className = 'black'
+  } else if (root.className === 'black') {
+    root.className = 'white'
+  }  else {
     root.className = 'light'
-  }
-  else {
-    root.className = 'dark'
   }
 }
 
