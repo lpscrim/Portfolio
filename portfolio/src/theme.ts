ded98ad9273ef11a5bf54e0e58008300fe14caec
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-03-04 13:55:28 +0000

    consolidated theme switch to Navbar

--- portfolio/src/theme.ts
+++ portfolio/src/theme.ts
@@ -1,6 +1,13 @@
-  export const  setTheme = (className: string) => {
+  export const  setTheme = () => {
   var root = document.getElementsByTagName('html')[0];
-  root.className = className;
+  if (root.className == null) {
+    root.className = 'dark'
+  } else if (root.className === 'dark') {
+    root.className = 'light'
+  }
+  else {
+    root.className = 'dark'
+  }
 }
 
 (window as any).setTheme = setTheme;
